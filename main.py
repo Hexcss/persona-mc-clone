a1b900d60cc4e472eee98edf5ceee4937b496e4c
javier.cader <javier.cader@ct.uneatlantico.es>
2024-05-20 17:29:58 +0200

    feat: Added Crosshair

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # main.py
 
-from ursina import Ursina, EditorCamera
+from ursina import Ursina, Entity, camera, color
 from scripts.player import Player
 from scripts.environment import create_environment
 
 app = Ursina()
 
-player = Player()
 ground = create_environment()
-editor_camera = EditorCamera()
+
+player = Player()
+
+crosshair = Entity(
+    parent=camera.ui,
+    model='quad',
+    color=color.white,
+    scale=(0.02, 0.02),
+    position=(0, 0)
+)
 
 app.run()
--- scripts/player.py
+++ scripts/player.py
@@ -1,22 +1,39 @@
 # scripts/player.py
 
-from ursina import Entity, color, Vec3, time, held_keys
+from ursina import Entity, Vec3, time, held_keys, camera, mouse, color
 
 class Player(Entity):
     def __init__(self):
         super().__init__()
-        self.model = 'cube'
-        self.color = color.orange
-        self.scale_y = 2
         self.position = Vec3(0, 1, 0)
+        self.speed = 5
+        self.rotation_speed = 100
+
+        # Hide the mouse cursor and lock it in the window
+        mouse.locked = True
+
+        # Attach camera to player
+        camera.parent = self
+        camera.position = (0, 1.8, 0)  # Position camera at player's eye level
+        camera.rotation = (0, 0, 0)    # Initial camera rotation
+        self.mouse_sensitivity = Vec3(40, 40, 40)
 
     def update(self):
-        speed = 5 * time.dt
-        if held_keys['a']:
-            self.x -= speed
-        if held_keys['d']:
-            self.x += speed
+        self.move()
+        self.rotate()
+
+    def move(self):
+        speed = self.speed * time.dt
         if held_keys['w']:
-            self.z += speed
+            self.position += self.forward * speed
         if held_keys['s']:
-            self.z -= speed
+            self.position -= self.forward * speed
+        if held_keys['a']:
+            self.position -= self.right * speed
+        if held_keys['d']:
+            self.position += self.right * speed
+
+    def rotate(self):
+        self.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[1] * time.dt
+        camera.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity[0] * time.dt
+        camera.rotation_x = max(min(camera.rotation_x, 90), -90)  # Limit vertical rotation to avoid flipping
